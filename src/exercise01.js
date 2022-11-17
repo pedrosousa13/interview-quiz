34114899f040b1967b52c76fe7b7f9ff85ce2480
Pedro Sousa <pedro.sousa@ef.com>
2022-11-17 09:24:56 +0100

    chore: exercise 1 refactor

--- src/exercise01.js
+++ src/exercise01.js
@@ -1,26 +1,11 @@
-import styles from "../styles/Home.module.css";
+const petName = "Leo";
+const placeholder = "{NAME}";
+const reminderTemplate =
+  "{NAME} is due for another visit. Please call us, so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
 
-const Exercise1 = () => {
+// What we want
+// Current output: "Leo is due for another visit. Please call us, so we can set up a new appointment. We look forward to seeing you and Leo soon.";
 
-  const petName = "Leo";
-  const placeholder = "{NAME}";
-  const reminderTemplate =
-    "{NAME} is due for another visit. Please call us, so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
+// There is a problem with `replace`, do you know what it is?
+const reminder = reminderTemplate.replace(placeholder, petName);
 
-  // There is a problem with `replace`, do you know what it is?
-  const reminder = reminderTemplate.replace(placeholder, petName);
-
-  // Current output: "Leo is due for another visit. Please call us, so we can set up a new appointment. We look forward to seeing you and {NAME} soon.";
-
-  // What we want
-  // Current output: "Leo is due for another visit. Please call us, so we can set up a new appointment. We look forward to seeing you and Leo soon.";
-
-  return (
-    <main className={styles.container}>
-      <h4>Exercise 1</h4>
-      <p>{reminder}</p>
-    </main>
-  );
-};
-
-export default Exercise1;
